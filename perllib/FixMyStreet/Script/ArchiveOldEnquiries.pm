ee2cc0c6b52ba9d6991e4e43424d83967a3292ad
Dave Arter <davea@mysociety.org>
2017-02-14 16:30:36 +0000

    Use env vars to control script behaviour; show progress

--- perllib/FixMyStreet/Script/ArchiveOldEnquiries.pm
+++ perllib/FixMyStreet/Script/ArchiveOldEnquiries.pm
@@ -11,11 +11,28 @@ use FixMyStreet::Cobrand;
 use FixMyStreet::Map;
 use FixMyStreet::Email;
 
+
+# Problems with this in their bodies_str will be closed
+my $bodies = $ENV{BODIES} || '2237';
+
+# What cobrand to use for the email templates
+my $cobrand_moniker = $ENV{COBRAND} || 'oxfordshire';
+
+# Limit the number of reports we'll close. Primarily for testing.
+my $rows = $ENV{ROWS} || undef;
+
+# Anything before this will be closed with no email
+my $closure_cutoff = $ENV{CLOSURE_CUTOFF} || "2015-01-01 00:00:00";
+
+# Anything last updated between $closure_cutoff and this will be closed with an
+# email sent to the reporter
+my $email_cutoff = $ENV{EMAIL_CUTOFF} || "2016-01-01 00:00:00";
+
 my $query = {
-    bodies_str => { 'LIKE', '%2237%'},
+    bodies_str => { 'LIKE', "%${bodies}%"},
     -and       => [
-      lastupdate => { '<', "2016-01-01 00:00:00" },
-      lastupdate => { '>', "2015-01-01 00:00:00" },
+      lastupdate => { '<', $email_cutoff },
+      lastupdate => { '>', $closure_cutoff },
     ],
     state      => [ FixMyStreet::DB::Result::Problem->open_states() ],
 };
@@ -25,6 +42,7 @@ sub archive {
     {
         distinct => 1,
         columns  => ['user_id'],
+        rows => $rows,
     })->all;
 
     @user_ids = map { $_->user_id } @user_ids;
@@ -33,17 +51,25 @@ sub archive {
         id => \@user_ids
     });
 
+    printf("%d users will receive closure emails.\n", $users->count);
+
     while ( my $user = $users->next ) {
         send_email_and_close($user);
     }
 
-    my $problems_2014 = FixMyStreet::DB->resultset('Problem')->search({
-        bodies_str => { 'LIKE', '%2237%'},
-        lastupdate => { '<', "2015-01-01 00:00:00" },
+    my $problems_to_close = FixMyStreet::DB->resultset('Problem')->search({
+        bodies_str => { 'LIKE', "%${bodies}%"},
+        lastupdate => { '<', $closure_cutoff },
         state      => [ FixMyStreet::DB::Result::Problem->open_states() ],
+    }, {
+        rows => $rows,
     });
 
-    $problems_2014->update({ state => 'closed' });
+    printf("Closing %d old reports, without sending emails: ", $problems_to_close->count);
+
+    $problems_to_close->update({ state => 'closed' });
+
+    printf("done.\n")
 }
 
 sub send_email_and_close {
@@ -55,7 +81,7 @@ sub send_email_and_close {
 
     return if scalar(@problems) == 0;
 
-    my $cobrand = FixMyStreet::Cobrand->get_class_for_moniker('oxfordshire')->new();
+    my $cobrand = FixMyStreet::Cobrand->get_class_for_moniker($cobrand_moniker)->new();
     $cobrand->set_lang_and_domain($problems[0]->lang, 1);
     FixMyStreet::Map::set_map_class($cobrand->map_type);
 
@@ -68,6 +94,7 @@ sub send_email_and_close {
     );
 
     # Send email
+    printf("Sending email about %d reports to user %d: ", scalar(@problems), $user->id);
     my $email_result = FixMyStreet::Email::send_cron(
         $problems->result_source->schema,
         'archive.txt',
@@ -81,5 +108,8 @@ sub send_email_and_close {
         $problems[0]->lang,
     );
 
+    printf("done.\nClosing reports: ");
+
     $problems->update({ state => 'closed' });
+    printf("done.\n");
 }
